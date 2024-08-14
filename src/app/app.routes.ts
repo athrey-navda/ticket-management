416906d6ade47532c6654889b28a40ec21cf5542
athrey-navda <athu.navada@gmail.com>
2024-08-14 09:58:14 -0400

    add: initial setup for my angular learnings add: basic login, navbar and herosection

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HerosectionComponent } from './components/herosection/herosection.component';
+import { LoginComponent } from './components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HerosectionComponent },
+  { path: 'login', component: LoginComponent },
+];
+
+export class AppRoutingModule {}
