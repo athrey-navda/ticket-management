934062aa7864c8ae6524272829d182a97245df34
athrey-navda <athu.navada@gmail.com>
2024-08-19 19:00:35 -0700

    add: signup component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { HerosectionComponent } from './components/herosection/herosection.component';
 import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 export const routes: Routes = [
   { path: '', component: HerosectionComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
 ];
 
 export class AppRoutingModule {}
